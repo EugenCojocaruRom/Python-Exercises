28e58b690a35a037ef81f9ebe5b10d65bcbe4760
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2026-04-02 10:14:41 +0300

    EX_02APR - Elysian Equation
    Set the values of variables x and y so that the result of the operation (x + y) * (x - y) is 21.

---
+++ EX_02APR - Elysian Equation/README.md
@@ -0,0 +1,4 @@
+EX\_02APR - Elysian Equation
+
+Set the values of variables x and y so that the result of the operation (x + y) \* (x - y) is 21.
+
---
+++ EX_02APR - Elysian Equation/main.py
@@ -0,0 +1,7 @@
+#Set values of variables
+x = 5
+y = 2
+#Set calculation method for result
+result = (x + y) * (x - y)
+#Print result
+print("Result =", result)
